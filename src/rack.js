4b974672d2006be52303385da5ac74eb02b3e13f
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-02-15 15:55:22 +0100

    test: rack move register third test Unit test is green

--- src/rack.js
+++ src/rack.js
@@ -1,5 +1,6 @@
 const {EMPTY_RACK} = require("../__tests__/mock/mockObjects");
 const colors = ["Y", "R"]
+const COLUMNS = [1, 2, 3, 4, 5, 6, 7]
 
 class Rack {
 
@@ -7,11 +8,15 @@ class Rack {
         return EMPTY_RACK
     }
 
-    placeToken(color) {
+    placeToken(color, column) {
         if(colors.indexOf(color) < 0) {
             return "Valid color - Y / R - required as first parameter"
         } else {
-            return null
+            if(COLUMNS.indexOf(column) < 0) {
+                return "Valid column - 1-7 - required as second parameter"
+            } else {
+                return null
+            }
         }
     }
 
