361b9f240e990ea0465f06ffa30856cce55dcc49
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-02-15 16:08:11 +0100

    chore: Refactor rack

--- src/rack.js
+++ src/rack.js
@@ -1,15 +1,16 @@
 const {EMPTY_RACK} = require("../__tests__/mock/mockObjects");
-const colors = ["Y", "R"]
-const COLUMNS = [1, 2, 3, 4, 5, 6, 7]
+const COLORS = ["Y", "R"]
+const COLUMNS = [1, 2, 3, 4, 5, 6, 7];
 
 class Rack {
+
 
     display () {
         return EMPTY_RACK
     }
 
     placeToken(color, column) {
-        if(colors.indexOf(color) < 0) {
+        if(COLORS.indexOf(color) < 0) {
             return "Valid color - Y / R - required as first parameter"
         } else {
             if(COLUMNS.indexOf(column) < 0) {
