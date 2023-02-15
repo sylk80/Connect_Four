ce34870d12733b6e379b0c3ba5880b5d38dedd68
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-02-15 14:52:55 +0100

    test: BDD test green

--- src/game.js
+++ src/game.js
@@ -3,11 +3,17 @@ const Rack = require("./rack.js")
 
 const startMessage = "Connect Four game starts"
 const creationMessage = "Empty rack is created."
+const lineSeparator = "\n"
 class Game {
 
 
     start() {
-        return startMessage + this.rack().display() + creationMessage
+        return lineSeparator +
+            startMessage +
+            lineSeparator + lineSeparator +
+            this.rack().display() +
+            lineSeparator + lineSeparator +
+            creationMessage
     }
 
     players() {
